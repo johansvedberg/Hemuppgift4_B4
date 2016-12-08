2ed316f625c70d168fb374ed1931aa5b0e9c8754
johansvedberg <johan@svedberg.biz>
2016-12-08 08:26:22 +0100

    OAEP_encode now works

--- src/main/OAEP.java
+++ src/main/OAEP.java
@@ -33,7 +33,11 @@ public class OAEP {
 			e.printStackTrace();
 		}
 
-		OAEP_encode("fd5507e917ecbe833878");
+		// String blala = "9b4bdfb2c796f1c16d0c0772a5848b67457e87891dbc8214";
+
+		// MGF(toByteArray(blala),21);
+
+		OAEP_encode("c107782954829b34dc531c14b40e9ea482578f988b719497aa0687");
 
 	}
 
@@ -51,7 +55,7 @@ public class OAEP {
 
 		byte[] output = new byte[maskLen];
 		System.arraycopy(T, 0, output, 0, output.length);
-		System.out.println(toHex(output));
+		// System.out.println(toHex(output));
 		return output;
 
 	}
@@ -59,11 +63,60 @@ public class OAEP {
 	private byte[] SHA1(byte[] mask, int i) {
 
 		digest.update(mask);
+
 		digest.update(new byte[3]);
 		digest.update((byte) i);
 		byte[] digestBytes = digest.digest();
 
 		return digestBytes;
+
+	}
+
+	private void OAEP_encode(String M) {
+		byte[] Mbytes = toByteArray(M);
+		// System.out.println(toHex(Mbytes));
+
+		String L = "";
+		digest2.update(toByteArray(L));
+		byte[] lHash = digest2.digest();
+		// int PSsize = (128 - M.length() - (2 * digest2.getDigestLength()) -
+		// 2);
+		byte[] PS = new byte[128 - Mbytes.length - 2 * digest2.getDigestLength() - 2];
+
+		byte[] pshash = concatenate(lHash, PS);
+		byte[] zeroOneMessage = concatenate(new byte[] { 0x01 }, Mbytes);
+
+		byte[] temp = concatenate(pshash, zeroOneMessage);
+
+		byte[] DB = new byte[128 - digest2.getDigestLength() - 1];
+
+		System.arraycopy(temp, 0, DB, 0, temp.length);
+
+		// DB = concatenate(zeroOneMessage, pshash);
+
+		String seedString = "1e652ec152d0bfcd65190ffc604c0933d0423381";
+		byte[] seed = toByteArray(seedString);
+
+		// System.out.println((128 - digest2.getDigestLength() - 1));
+		byte[] dbMask = MGF(seed, (128 - digest2.getDigestLength() - 1));
+
+		byte[] maskedDB = xor(DB, dbMask);
+
+		byte[] seedMask = MGF(maskedDB, digest2.getDigestLength());
+
+		byte[] maskedSeed = xor(seed, seedMask);
+
+		byte[] almost = concatenate(new byte[] { 0x00 }, maskedSeed);
+
+		byte[] EM = concatenate(almost, maskedDB);
+
+		// System.out.println(toHex(maskedDB));
+
+		System.out.println(toHex(EM));
+
+	}
+
+	private void OAEP_decode() {
 
 	}
 
@@ -82,72 +135,19 @@ public class OAEP {
 		return DatatypeConverter.parseHexBinary(s);
 	}
 
-	private void OAEP_encode(String M) {
-		byte[] Mbytes = toByteArray(M);
-
-		String L = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
-		//digest2.update(toByteArray(L));
-		byte[] lHash = toByteArray(L);
-		// int PSsize = (128 - M.length() - (2 * digest2.getDigestLength()) -
-		// 2);
-		byte[] PS = new byte[128 - M.length() - 2 * digest2.getDigestLength() - 2];
-		// Arrays.fill(ps, (byte) 00);
-
-		byte[] pshash = concatenate(lHash, PS);
-		byte[] zeroOneMessage = concatenate(new byte[] { 0x01 }, Mbytes);
-		
-
-		int DBLength = 128 - digest2.getDigestLength() - 1;
-		
-		//System.out.println(DBLength);
-
-		byte[] DB = new byte[DBLength];
-		
-		byte[]temp = concatenate(pshash, zeroOneMessage);
-		
-		System.arraycopy(temp, 0, DB, 0, temp.length);
-
-		//DB = concatenate(zeroOneMessage, pshash);
-
-		String seedString = "1e652ec152d0bfcd65190ffc604c0933d0423381";
-		byte[] seed = toByteArray(seedString);
-		//System.out.println((128 - digest2.getDigestLength() - 1));
-		byte[] dbMask = MGF(seed, (128 - digest2.getDigestLength() - 1));
-
-		byte[] maskedDB = xor(DB, dbMask);
-		
-		byte[] seedMask = MGF(maskedDB, digest2.getDigestLength());
-
-		byte[] maskedSeed = xor(seed, seedMask);
-
-
-		byte[] almost = concatenate(new byte[] { 0x00 }, maskedSeed);
-
-		byte[] EM = concatenate(almost, maskedDB);
-
-		System.out.println(toHex(EM));
-
-	}
-
 	private byte[] xor(byte[] a, byte[] b) {
-		
+
 		if (a.length != b.length) {
 			throw new InternalError("Byte a must equal Byte b");
 		}
 
-		// Create a temporary array to store results in
 		byte[] output = new byte[a.length];
 
-		// Loop through the array and xor each bit
 		for (int i = 0; i < output.length; i++) {
 			output[i] = (byte) (a[i] ^ b[i]);
 		}
 
 		return output;
-	}
-
-	private void OAEP_decode() {
-
 	}
 
 }
